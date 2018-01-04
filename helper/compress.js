da6fbbdf6c6695a19ce418b24793bc6974d6606f
stein <1504344009@qq.com>
2018-01-04 13:33:21 +0800

    delete four g for acceptEncoding

--- helper/compress.js
+++ helper/compress.js
@@ -1,13 +1,13 @@
 const {createGzip,createDeflate} = require('zlib');
 
 module.exports = (rs,req,res)=>{
-    const acceptEngcoding = req.headers['accept-engcoding'];
-    if(!acceptEngcoding||!acceptEngcoding.match(/\b(gzip|deflate)\b/)){
+    const acceptEncoding = req.headers['accept-encoding'];
+    if(!acceptEncoding||!acceptEncoding.match(/\b(gzip|deflate)\b/)){
         return rs;
-    }else if(acceptEngcoding.match(/\bgzip\b/)){
+    }else if(acceptEncoding.match(/\bgzip\b/)){
         res.setHeader('Content-Encoding','gzip');
         return rs.pipe(createGzip());
-    }else if(acceptEngcoding.match(/\bdeflate\b/)){
+    }else if(acceptEncoding.match(/\bdeflate\b/)){
         res.setHeader('Content-Encoding','deflate');
         return rs.pipe(createDeflate());
     }
